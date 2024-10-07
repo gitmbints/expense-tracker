a65efbc4644b88b4931482a5577d4cbf79179342
Ambinintsoa <amr@alternateeve.com>
2024-10-07 16:53:45 +0300

    feat: add primeng for ui-tool-kit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeNGConfig } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'expense-tracker';
+export class AppComponent implements OnInit {
+  title = 'Expense Tracker';
+
+  primeConfig: PrimeNGConfig = inject(PrimeNGConfig);
+
+  ngOnInit(): void {
+    this.primeConfig.ripple = true;
+  }
 }

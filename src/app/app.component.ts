97918a8e155a5e6c24557695277f74dd852abe97
Ambinintsoa <amr@alternateeve.com>
2024-10-08 17:27:48 +0300

    add flowbite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   title: string = 'Expense Tracker';
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }

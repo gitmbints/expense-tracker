45143ee42388d08bc3a98266ab619589cb0573cb
Ambinintsoa <amr@alternateeve.com>
2024-10-09 16:03:05 +0300

    Setting up routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ExpensesComponent } from './components/expenses/expenses.component';
+import { IncomesComponent } from './components/incomes/incomes.component';
+import { SavingsComponent } from './components/savings/savings.component';
+import { InvestComponent } from './components/invest/invest.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'expenses',
+    component: ExpensesComponent,
+  },
+  {
+    path: 'incomes',
+    component: IncomesComponent,
+  },
+  {
+    path: 'savings',
+    component: SavingsComponent,
+  },
+  {
+    path: 'invest',
+    component: InvestComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
+  },
+];

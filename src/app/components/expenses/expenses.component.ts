9e52fc9811a3172abe36e0a33bbef6959107461f
gitmbints <rahmbints@gmail.com>
2024-10-14 23:36:58 +0300

    feat: get the dropdown data from view

--- src/app/components/expenses/expenses.component.ts
+++ src/app/components/expenses/expenses.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject, OnInit, Signal } from '@angular/core';
 import { ExpenseService } from '../../services/expense/expense.service';
 import { Expense } from '../../model/expense';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-expenses',
@@ -22,9 +22,26 @@ export class ExpensesComponent {
   expenseForm = this.formBuilder.group({
     name: [''],
     amount: [0],
-    category: [''],
+    category: this.formBuilder.array([]),
     date: [''],
   });
+
+  private getCategory(): FormArray {
+    return this.expenseForm.get('category') as FormArray;
+  }
+
+  onCategoryChange(event: any): void {
+    const selectedCategory = this.getCategory();
+
+    if (event.target.checked) {
+      selectedCategory.push(this.formBuilder.control(event.target.value));
+    } else {
+      const index = selectedCategory.controls.findIndex(
+        (ctrl) => ctrl.value === event.target.value,
+      );
+      selectedCategory.removeAt(index);
+    }
+  }
 
   onSubmit(): void {
     console.warn(this.expenseForm.value);

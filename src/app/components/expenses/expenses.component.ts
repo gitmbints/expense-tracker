85a3e86cf246894665dc1cfbd7a2b45f215a7013
Ambinintsoa <amr@alternateeve.com>
2024-10-21 16:35:05 +0300

    feat: bind datepicker into form model

--- src/app/components/expenses/expenses.component.ts
+++ src/app/components/expenses/expenses.component.ts
@@ -2,20 +2,14 @@ import { Component, inject, OnInit, Signal } from '@angular/core';
 import { ExpenseService } from '../../services/expense/expense.service';
 import { Expense } from '../../model/expense';
 import {
-  FormArray,
-  FormBuilder,
   FormControl,
+  FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
 import { Flowbite } from '../../flowbite/flowbite';
+import { Datepicker } from 'flowbite';
 
-interface ExpenseForm {
-  name: FormControl<string>;
-  amount: FormControl<number>;
-  category: FormArray<FormControl>;
-  date: FormControl<string>;
-}
 @Component({
   selector: 'app-expenses',
   standalone: true,
@@ -24,63 +18,44 @@ interface ExpenseForm {
   styleUrl: './expenses.component.css',
 })
 @Flowbite()
-export class ExpensesComponent {
-  title: string = 'Dépenses';
+export class ExpensesComponent implements OnInit {
+  readonly title: string = 'Dépenses';
   expenseList: Signal<Expense[]>;
   expenseCategoryList: string[];
 
   expenseService: ExpenseService = inject(ExpenseService);
-  formBuilder: FormBuilder = inject(FormBuilder);
 
   constructor() {
     this.expenseList = this.expenseService.getExpenseList();
     this.expenseCategoryList = this.expenseService.getExpenseCategoryList();
   }
 
-  expenseForm = this.formBuilder.group<ExpenseForm>({
-    name: this.formBuilder.control<string>('', {
-      validators: Validators.required,
-      nonNullable: true,
-    }),
-    amount: this.formBuilder.control<number>(0, {
-      validators: [Validators.required, Validators.min(0)],
-      nonNullable: true,
-    }),
-    category: this.formBuilder.array([]),
-    date: this.formBuilder.control<string>('', {
-      validators: Validators.required,
-      nonNullable: true,
-    }),
+  ngOnInit(): void {
+    this.initDatePicker();
+  }
+
+  expenseForm = new FormGroup({
+    name: new FormControl(''),
+    amount: new FormControl(0),
+    date: new FormControl(''),
   });
 
-  private getCategory(): FormArray {
-    return this.expenseForm.get('category') as FormArray;
-  }
+  private initDatePicker(): void {
+    setTimeout(() => {
+      const datePickerElement = document.getElementById('datepicker-actions');
+      new Datepicker(datePickerElement);
 
-  onCategoryChange(event: Event): void {
-    const selectedCategory = this.getCategory();
-    const checkboxElement = event.target as HTMLInputElement;
-
-    if (checkboxElement.checked) {
-      selectedCategory.push(this.formBuilder.control(checkboxElement.value));
-    } else {
-      const index = selectedCategory.controls.findIndex(
-        (ctrl) => ctrl.value === checkboxElement.value,
-      );
-      selectedCategory.removeAt(index);
-    }
-  }
-
-  onDateChange(event: Event): void {
-    const input = event.target as HTMLInputElement;
-    const dateValue = input.value; // Expected format: YYYY-MM-DD
-    this.expenseForm.patchValue({ date: dateValue });
+      datePickerElement?.addEventListener('changeDate', (e: any) => {
+        const value = e.target.value;
+        const formControl = this.expenseForm.controls.date;
+        formControl?.setValue(value);
+        formControl?.markAsDirty;
+      });
+    });
   }
 
   onSubmit(): void {
-    const expenseData = this.expenseForm.getRawValue();
-    this.expenseService.addExpense(expenseData);
-
-    console.warn(this.expenseForm.value);
+    console.log(this.expenseForm.value);
+    this.expenseForm.reset();
   }
 }

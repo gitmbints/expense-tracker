17aa4a9e4ffed03a314d71d7c4b01df1626a251d
Ambinintsoa <amr@alternateeve.com>
2024-12-03 17:44:37 +0300

    feat: set dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Signal } from '@angular/core';
+import { Component, computed, inject, Signal } from '@angular/core';
 import { IncomeService } from '../../services/income/income.service';
 import { ExpenseService } from '../../services/expense/expense.service';
 
@@ -13,6 +13,7 @@ export class DashboardComponent {
   title: string = 'Dashboard';
   readonly totalIncome: Signal<number>;
   readonly totalExpense: Signal<number>;
+  readonly remaining: Signal<number>;
 
   incomeService: IncomeService = inject(IncomeService);
   expenseService: ExpenseService = inject(ExpenseService);
@@ -20,5 +21,8 @@ export class DashboardComponent {
   constructor() {
     this.totalIncome = this.incomeService.totalIncome;
     this.totalExpense = this.expenseService.totalExpense;
+    this.remaining = computed(() => {
+      return this.totalIncome() - this.totalExpense();
+    });
   }
 }

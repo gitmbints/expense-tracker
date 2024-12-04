5158282f67d1bf9928ed455ce7550b51e0b18905
Ambinintsoa <amr@alternateeve.com>
2024-12-04 17:33:53 +0300

    feat: set chart on the app

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,25 @@
-import { Component, computed, inject, Signal } from '@angular/core';
+import { Component, computed, inject, Signal, ViewChild } from '@angular/core';
 import { IncomeService } from '../../services/income/income.service';
 import { ExpenseService } from '../../services/expense/expense.service';
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexTitleSubtitle,
+  ApexXAxis,
+  ChartComponent,
+  NgApexchartsModule,
+} from 'ng-apexcharts';
 
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  title: ApexTitleSubtitle;
+};
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [NgApexchartsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
@@ -14,6 +28,9 @@ export class DashboardComponent {
   readonly totalIncome: Signal<number>;
   readonly totalExpense: Signal<number>;
   readonly remaining: Signal<number>;
+
+  @ViewChild('chart', { static: false }) chart!: ChartComponent;
+  public chartOptions: ChartOptions;
 
   incomeService: IncomeService = inject(IncomeService);
   expenseService: ExpenseService = inject(ExpenseService);
@@ -24,5 +41,35 @@ export class DashboardComponent {
     this.remaining = computed(() => {
       return this.totalIncome() - this.totalExpense();
     });
+
+    this.chartOptions = {
+      series: [
+        {
+          name: 'My-series',
+          data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
+        },
+      ],
+      chart: {
+        width: 350,
+        height: 350,
+        type: 'bar',
+      },
+      title: {
+        text: 'My First Angular Chart',
+      },
+      xaxis: {
+        categories: [
+          'Jan',
+          'Feb',
+          'Mar',
+          'Apr',
+          'May',
+          'Jun',
+          'Jul',
+          'Aug',
+          'Sep',
+        ],
+      },
+    };
   }
 }

1c42c3301b43216e59418b5c4a8fc63a64aa6373
Ambinintsoa <amr@alternateeve.com>
2025-02-04 17:35:59 +0300

    Get user metadata with the authentication

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,16 +1,27 @@
-import { Component, inject } from "@angular/core";
+import { Component, inject, OnInit, Signal } from "@angular/core";
 import { Router, RouterLink } from "@angular/router";
+import { AuthService } from "../../services/auth/auth.service";
+import { toSignal } from "@angular/core/rxjs-interop";
+import { NgOptimizedImage } from "@angular/common";
+import { User } from "@supabase/supabase-js";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgOptimizedImage],
   templateUrl: './sidebar.component.html',
 })
 export class SidebarComponent {
   router = inject(Router);
+  private authService = inject(AuthService);
+  user: Signal<User | null> = toSignal(this.authService.user$, { initialValue: null });
+
+  constructor() {
+    console.log(this.user());
+  }
 
   logout(): void {
-    this.router.navigate(['/auth']).then(r => console.log(r));
+    this.authService.signOut();
+    this.router.navigate(['/auth']);
   }
 }

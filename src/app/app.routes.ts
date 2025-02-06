c4b84c5a5d039017cd74d9aa1a7d470f941fabd6
Ambinintsoa <amr@alternateeve.com>
2025-02-06 17:43:24 +0300

    login and logout with google and supabase

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,8 +6,14 @@ import { SavingsComponent } from './components/savings/savings.component';
 import { InvestComponent } from './components/invest/invest.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { AuthComponent } from "./components/auth/auth.component";
+import { authGuard } from "./guards/auth.guard";
 
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
+  },
   {
     path: 'auth',
     component: AuthComponent,
@@ -15,27 +21,27 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
+    canActivate: [authGuard],
   },
   {
     path: 'expenses',
     component: ExpensesComponent,
+    canActivate: [authGuard],
   },
   {
     path: 'incomes',
     component: IncomesComponent,
+    canActivate: [authGuard],
   },
   {
     path: 'savings',
     component: SavingsComponent,
+    canActivate: [authGuard],
   },
   {
     path: 'invest',
     component: InvestComponent,
-  },
-  {
-    path: '',
-    redirectTo: '/dashboard',
-    pathMatch: 'full',
+    canActivate: [authGuard],
   },
   {
     path: '**',

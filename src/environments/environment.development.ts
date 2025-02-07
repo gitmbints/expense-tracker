c2f83c1be112b3fe575cc7e9f0d0fe00da3d3bfe
Ambinintsoa <amr@alternateeve.com>
2025-02-07 16:42:23 +0300

    modif env file to adapt to environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,4 +3,5 @@ export const environment = {
   supabaseUrl: 'https://fgwogftxkjgjerjnjyqk.supabase.co',
   supabaseKey:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnd29nZnR4a2pnamVyam5qeXFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAyMDgyMjUsImV4cCI6MjA0NTc4NDIyNX0.vDw9fzwWxkC4hvza3SlsZacQzr-aLqa5EWdpBSqvec0',
+  redirectUrl: 'http://localhost:4200'
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://fgwogftxkjgjerjnjyqk.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnd29nZnR4a2pnamVyam5qeXFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAyMDgyMjUsImV4cCI6MjA0NTc4NDIyNX0.vDw9fzwWxkC4hvza3SlsZacQzr-aLqa5EWdpBSqvec0',
+  redirectUrl: 'https://gitmbints.github.io/expense-tracker/'
+};
